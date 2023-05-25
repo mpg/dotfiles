32e6d67953369b0c34882cf372069a862e8cbd89
Manuel Pégourié-Gonnard <mpg@elzevir.fr>
2023-05-25 17:06:01 +0200

    nvim: adapt deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,18 +2,19 @@
 
 MYNAME=deploy.sh
 README=Readme.md
+NVIM=nvim
 
-# location of the checkout, relative to HOME
-DIR=etc
+# absolute path to the checkout
+DIR="$HOME/etc"
 
-if cd $HOME/$DIR && test -x $MYNAME -a -r $README; then :; else
+if cd $DIR && test -x $MYNAME -a -r $README; then :; else
     echo "Please adjust DIR!" 2>&1
     exit 1
 fi
 
 link() {
     file=$1
-    link=$HOME/.$1
+    link=$2
 
     if [ -L $link ]; then
         echo "$file already linked, skipping"
@@ -21,7 +22,7 @@ link() {
     fi
 
     if [ -e $link ]; then
-        echo "$file exists, backing up to $file.bak"
+        echo "$file exists, backing up to $PWD/$file.bak"
         mv $link $file.bak
     fi
 
@@ -29,11 +30,13 @@ link() {
     echo "$file linked"
 }
 
-FILES=$(ls | grep -v $MYNAME'\|'$README)
+FILES=$(ls | grep -v $MYNAME'\|'$README'\|'$NVIM)
 
 for f in $FILES; do
-    link $f
+    link $f $HOME/.$f
 done
+
+link $NVIM $HOME/.config/$NVIM
 
 mkdir -p "$HOME"/.cache/vim/undofiles
 echo "vim undofiles directory created"
