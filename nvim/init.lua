afec07dceb83df3d8f9613dbf2ce5087251b78ac
Manuel Pégourié-Gonnard <mpg@elzevir.fr>
2025-02-15 09:33:28 +0100

    nvim: more LSP/rust settings
    Mostly from https://github.com/jonhoo/configs/blob/master/editor/.config/nvim/init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -29,6 +29,13 @@ vim.opt.foldlevelstart = 999
 vim.keymap.set("n", "<Space>", "<Nop>", { silent = true })
 vim.g.mapleader = " "
 
+-- Language settings
+-- -----------------
+
+-- Rust: see :help rust
+vim.g.rustfmt_autosave = 1
+vim.g.rustfmt_fail_silently = 1
+
 -- Shortcuts
 -- ---------
 
@@ -112,10 +119,118 @@ require("lazy").setup({
             -- Rust
             lspconfig.rust_analyzer.setup {
                 -- Server-specific settings. See `:help lspconfig-setup`
+                -- https://rust-analyzer.github.io/book/configuration.html
                 settings = {
                     ['rust-analyzer'] = {},
                 }
             }
+
+            -- Global mappings.
+            -- See `:help vim.diagnostic.*` for documentation on any of the below functions
+            vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+            vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+            vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+            vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+
+            -- Use LspAttach autocommand to only map the following keys
+            -- after the language server attaches to the current buffer
+            vim.api.nvim_create_autocmd('LspAttach', {
+                group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+                callback = function(ev)
+                    -- Enable completion triggered by <c-x><c-o>
+                    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+                    -- Buffer local mappings.
+                    -- See `:help vim.lsp.*` for documentation on any of the below functions
+                    local opts = { buffer = ev.buf }
+                    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+                    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+                    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+                    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+                    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+                    vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+                    vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+                    vim.keymap.set('n', '<leader>wl', function()
+                        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                    end, opts)
+                    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+                    vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, opts)
+                    vim.keymap.set({ 'n', 'v' }, '<leader>a', vim.lsp.buf.code_action, opts)
+                    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+                    vim.keymap.set('n', '<leader>f', function()
+                        vim.lsp.buf.format { async = true }
+                    end, opts)
+
+                    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+
+                    -- None of this semantics tokens business.
+                    client.server_capabilities.semanticTokensProvider = nil
+                end,
+            })
+        end
+    },
+    -- LSP-based code-completion
+    {
+        "hrsh7th/nvim-cmp",
+        -- load cmp on InsertEnter
+        event = "InsertEnter",
+        -- these dependencies will only be loaded when cmp loads
+        -- dependencies are always lazy-loaded unless specified otherwise
+        dependencies = {
+            'neovim/nvim-lspconfig',
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+        },
+        config = function()
+            local cmp = require'cmp'
+            cmp.setup({
+                snippet = {
+                    -- REQUIRED by nvim-cmp. get rid of it once we can
+                    expand = function(args)
+                        vim.fn["vsnip#anonymous"](args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<C-e>'] = cmp.mapping.abort(),
+                    -- Accept currently selected item.
+                    -- Set `select` to `false` to only confirm explicitly selected items.
+                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                }),
+                sources = cmp.config.sources({
+                    { name = 'nvim_lsp' },
+                }, {
+                    { name = 'path' },
+                }),
+                experimental = {
+                    ghost_text = true,
+                },
+            })
+
+            -- Enable completing paths in :
+            cmp.setup.cmdline(':', {
+                sources = cmp.config.sources({
+                    { name = 'path' }
+                })
+            })
+        end
+    },
+    -- inline function signatures
+    {
+        "ray-x/lsp_signature.nvim",
+        event = "VeryLazy",
+        opts = {},
+        config = function(_, opts)
+            -- Get signatures (and _only_ signatures) when in argument lists.
+            require "lsp_signature".setup({
+                doc_lines = 0,
+                handler_opts = {
+                    border = "none"
+                },
+            })
         end
     },
 })
